e9169f19e387b681063450226c6188766ff2e9bf
George Gastaldi <gegastaldi@gmail.com>
2022-10-26 12:34:45 -0300

    Add build scripts (#13)
    * Add build scripts
    
    - Fixes #3
    - Fixes #7
    
    * Fix parent pom.xml
    
    * Added docs
    
    * Add integration tests
    
    * Fix extension

---
+++ integration-tests/src/test/java/io/quarkiverse/jdbi/it/JdbiResourceTest.java
@@ -0,0 +1,21 @@
+package io.quarkiverse.jdbi.it;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class JdbiResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/jdbi")
+                .then()
+                .statusCode(200)
+                .body(is("Hello jdbi"));
+    }
+}
