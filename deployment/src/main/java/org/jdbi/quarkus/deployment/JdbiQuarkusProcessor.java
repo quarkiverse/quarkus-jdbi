3d53493b19222ecf8f32f743bee2870e6d8d7c92
Tamas <cserveny.tamas@gmail.com>
2025-08-11 21:38:01 +0200

    Update JdbiQuarkusProcessor.java to support more classes (#206)
    * Update JdbiQuarkusProcessor.java to support more classes
    
    * add Config caches and transaction decorator
    
    * Refactor reflection handling in JdbiQuarkusProcessor to use builder pattern for ReflectiveClassBuildItem
    
    * Refactor formatting in JdbiQuarkusProcessor for improved readability

--- deployment/src/main/java/org/jdbi/quarkus/deployment/JdbiQuarkusProcessor.java
+++ deployment/src/main/java/org/jdbi/quarkus/deployment/JdbiQuarkusProcessor.java
@@ -35,7 +35,7 @@ class JdbiQuarkusProcessor {
     @BuildStep
     ReflectiveClassBuildItem reflectionConstructorOnly() {
         // since we only need reflection to the constructor of the class, we can specify `false` for both the methods and the fields arguments.
-        return new ReflectiveClassBuildItem(false, false,
+        return ReflectiveClassBuildItem.builder(
                 "org.jdbi.v3.core.transaction.SerializableTransactionRunner$Configuration",
                 "org.jdbi.v3.core.config.JdbiCaches",
                 "org.jdbi.v3.core.statement.SqlStatements",
@@ -47,30 +47,43 @@ class JdbiQuarkusProcessor {
                 "org.jdbi.v3.core.mapper.MapMappers",
                 "org.jdbi.v3.core.mapper.MapEntryMappers",
                 "org.jdbi.v3.core.mapper.reflect.ReflectionMappers",
+                "org.jdbi.v3.core.mapper.immutables.JdbiImmutables",
                 "org.jdbi.v3.core.mapper.reflect.internal.PojoTypes",
                 "org.jdbi.v3.core.collector.JdbiCollectors",
+                "org.jdbi.v3.core.mapper.freebuilder.JdbiFreeBuilders",
                 "org.jdbi.v3.core.qualifier.Qualifiers",
                 "org.jdbi.v3.core.result.ResultProducers",
                 "org.jdbi.v3.core.array.SqlArrayTypes",
                 "org.jdbi.v3.core.extension.Extensions",
                 "org.jdbi.v3.core.internal.OnDemandExtensions",
                 "org.jdbi.v3.core.internal.EnumStrategies",
+                "org.jdbi.v3.sqlobject.transaction.internal.TransactionDecorator",
+                "org.jdbi.v3.core.config.internal.ConfigCaches",
                 "org.jdbi.v3.core.enums.Enums",
                 "org.jdbi.v3.core.Handles",
                 "org.jdbi.v3.sqlobject.Handlers",
                 "org.jdbi.v3.sqlobject.HandlerDecorators",
                 "org.jdbi.v3.sqlobject.statement.internal.SqlObjectStatementConfiguration",
+                "org.jdbi.v3.sqlobject.customizer.TimestampedConfig",
                 "org.jdbi.v3.sqlobject.SqlObjects",
                 "com.github.benmanes.caffeine.cache.PSMS",
-                "com.github.benmanes.caffeine.cache.SSMS",
-                "org.jdbi.v3.postgres.PostgresTypes");
+                "org.jdbi.v3.postgres.PostgresTypes")
+                .constructors(true)
+                .methods(false)
+                .fields(false)
+                .build();
     }
 
     @BuildStep
     ReflectiveClassBuildItem reflectionMethods() {
-        return new ReflectiveClassBuildItem(true, false,
+        return ReflectiveClassBuildItem.builder(
                 "com.github.benmanes.caffeine.cache.CacheLoader",
-                "org.jdbi.v3.sqlobject.SqlObject");
+                "com.github.benmanes.caffeine.cache.SSMS",
+                "org.jdbi.v3.sqlobject.SqlObject")
+                .constructors(false)
+                .methods(true)
+                .fields(false)
+                .build();
     }
 
     @BuildStep
@@ -78,10 +91,14 @@ class JdbiQuarkusProcessor {
             CombinedIndexBuildItem index,
             BuildProducer<ReflectiveClassBuildItem> reflectionClasses) {
 
-        DotName jdbiConfig = DotName.createSimple("org.jdbi.v3.core.config.JdbiConfig");
-        index.getIndex().getAllKnownImplementors(jdbiConfig)
+        index.getIndex().getAllKnownImplementations("org.jdbi.v3.core.config.JdbiConfig")
                 .forEach(info -> {
-                    reflectionClasses.produce(new ReflectiveClassBuildItem(true, true, false, info.name().toString()));
+                    reflectionClasses.produce(
+                            ReflectiveClassBuildItem.builder(info.name().toString())
+                                    .constructors(true)
+                                    .methods(true)
+                                    .fields(false)
+                                    .build());
                 });
     }
 
@@ -90,14 +107,17 @@ class JdbiQuarkusProcessor {
             BuildProducer<ReflectiveClassBuildItem> reflectionClasses,
             BuildProducer<ServiceProviderBuildItem> serviceProviders) {
 
-        DotName plugin = DotName.createSimple(PLUGIN);
-
         Collection<String> plugins = new ArrayList<>();
-        index.getIndex().getAllKnownImplementors(plugin)
+        index.getIndex().getAllKnownImplementations(PLUGIN)
                 .forEach(info -> {
                     String pluginName = info.name().toString();
                     plugins.add(pluginName);
-                    reflectionClasses.produce(new ReflectiveClassBuildItem(true, true, false, pluginName));
+                    reflectionClasses.produce(
+                            ReflectiveClassBuildItem.builder(pluginName)
+                                    .constructors(true)
+                                    .methods(true)
+                                    .fields(false)
+                                    .build());
                 });
 
         serviceProviders.produce(new ServiceProviderBuildItem(PLUGIN, plugins));
