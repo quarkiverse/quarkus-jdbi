e8fc31116ecb5b6cbfa6c6de78d624260052a7af
Tamas <cserveny.tamas@gmail.com>
2025-12-04 20:55:26 +0100

    simplify reflection class registration (3.49+) (#228)

--- deployment/src/main/java/org/jdbi/quarkus/deployment/JdbiQuarkusProcessor.java
+++ deployment/src/main/java/org/jdbi/quarkus/deployment/JdbiQuarkusProcessor.java
@@ -32,40 +32,10 @@ class JdbiQuarkusProcessor {
 
     @BuildStep
     ReflectiveClassBuildItem reflectionConstructorOnly() {
-        // since we only need reflection to the constructor of the class, we can specify `false` for both the methods and the fields arguments.
+        // since we only need reflection to the constructor of the class,
+        // we can specify `false` for both the methods and the fields arguments.
         return ReflectiveClassBuildItem.builder(
-                "org.jdbi.v3.core.transaction.SerializableTransactionRunner$Configuration",
-                "org.jdbi.v3.core.config.JdbiCaches",
-                "org.jdbi.v3.core.statement.SqlStatements",
-                "org.jdbi.v3.core.statement.StatementExceptions",
-                "org.jdbi.v3.core.argument.Arguments",
-                "org.jdbi.v3.core.mapper.RowMappers",
-                "org.jdbi.v3.core.mapper.ColumnMappers",
-                "org.jdbi.v3.core.mapper.Mappers",
-                "org.jdbi.v3.core.mapper.MapMappers",
-                "org.jdbi.v3.core.mapper.MapEntryMappers",
-                "org.jdbi.v3.core.mapper.reflect.ReflectionMappers",
-                "org.jdbi.v3.core.mapper.immutables.JdbiImmutables",
-                "org.jdbi.v3.core.mapper.reflect.internal.PojoTypes",
-                "org.jdbi.v3.core.collector.JdbiCollectors",
-                "org.jdbi.v3.core.mapper.freebuilder.JdbiFreeBuilders",
-                "org.jdbi.v3.core.qualifier.Qualifiers",
-                "org.jdbi.v3.core.result.ResultProducers",
-                "org.jdbi.v3.core.array.SqlArrayTypes",
-                "org.jdbi.v3.core.extension.Extensions",
-                "org.jdbi.v3.core.internal.OnDemandExtensions",
-                "org.jdbi.v3.core.internal.EnumStrategies",
-                "org.jdbi.v3.sqlobject.transaction.internal.TransactionDecorator",
-                "org.jdbi.v3.core.config.internal.ConfigCaches",
-                "org.jdbi.v3.core.enums.Enums",
-                "org.jdbi.v3.core.Handles",
-                "org.jdbi.v3.sqlobject.Handlers",
-                "org.jdbi.v3.sqlobject.HandlerDecorators",
-                "org.jdbi.v3.sqlobject.statement.internal.SqlObjectStatementConfiguration",
-                "org.jdbi.v3.sqlobject.customizer.TimestampedConfig",
-                "org.jdbi.v3.sqlobject.SqlObjects",
-                "com.github.benmanes.caffeine.cache.PSMS",
-                "org.jdbi.v3.postgres.PostgresTypes")
+                "com.github.benmanes.caffeine.cache.PSMS")
                 .constructors(true)
                 .methods(false)
                 .fields(false)
@@ -76,8 +46,7 @@ class JdbiQuarkusProcessor {
     ReflectiveClassBuildItem reflectionMethods() {
         return ReflectiveClassBuildItem.builder(
                 "com.github.benmanes.caffeine.cache.CacheLoader",
-                "com.github.benmanes.caffeine.cache.SSMS",
-                "org.jdbi.v3.sqlobject.SqlObject")
+                "com.github.benmanes.caffeine.cache.SSMS")
                 .constructors(false)
                 .methods(true)
                 .fields(false)
